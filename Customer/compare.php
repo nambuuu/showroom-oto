7728b0c9af9a5f769a3999de48458ca13e96f305
AnhNguyen178 <nguyenanhXXCH@gmail.com>
2026-05-21 23:17:53 +0700

    Thêm chức năng so sánh

--- Customer/compare.php
+++ Customer/compare.php
@@ -5,7 +5,7 @@ require_once '../config/db.php';
 <html lang="vi">
 <head>
     <meta charset="UTF-8">
-    <meta name="iewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Viewport" content="width=device-width, initial-scale=1.0">
     <title>AutoSuperCar | So Sánh Xe</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Orbitron:wght@700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
